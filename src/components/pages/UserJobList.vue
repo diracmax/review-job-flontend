ae528a1174f7498a43a2f9ed413d5edc87855066
diracmax <you@example.com>
2022-06-17 22:19:59 +0900

    コンポーネントの使用例を追加

--- src/components/pages/UserJobList.vue
+++ src/components/pages/UserJobList.vue
@@ -1,9 +1,14 @@
 <template>
     <div>仕事リスト</div>
+    <review-overview></review-overview>
 </template>
 
 <script>
+import ReviewOverview from '../modules/ReviewOverview';
 export default {
+  components: {
+    ReviewOverview
+  },
   name: 'UserJobList',
   props: {
   }
