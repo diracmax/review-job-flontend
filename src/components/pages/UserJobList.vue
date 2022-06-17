bc793d71634beac397a776e584b6478cdf27ce8c
diracmax <you@example.com>
2022-06-17 22:06:37 +0900

    ページの雛形追加

---
+++ src/components/pages/UserJobList.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>仕事リスト</div>
+</template>
+
+<script>
+export default {
+  name: 'UserJobList',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/router.js
+++ src/router.js
@@ -1,12 +1,36 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import MemberList from './components/MemberList.vue'
+import MemberList from './components/pages/MemberList.vue'
+import UserJobList from './components/pages/UserJobList.vue'
+import JobContent from './components/pages/JobContent.vue'
+import CategorySearch from './components/pages/CategorySearch.vue'
+import ReviewPost from './components/pages/ReviewPost.vue'
 
 
 const routes = [
   {
-    path: '/members',
+    path: '/groups/:groupId/members',
     name: 'Members',
     component: MemberList,
+  },
+  {
+    path: '/groups/:groupId/members/:userId/jobs',
+    name: 'UserJobs',
+    component: UserJobList,
+  },
+  {
+    path: '/groups/:groupId/members/:userId/jobs/:jobId',
+    name: 'JobContent',
+    component: JobContent,
+  },
+  {
+    path: '/category',
+    name: 'CategorySearch',
+    component: CategorySearch,
+  },
+  {
+    path: '/groups/:groupId/review',
+    name: 'ReviewPost',
+    component: ReviewPost,
   },
 ]
 
