60363bda71f8af604da6e03f88a84368413c4b21
diracmax <you@example.com>
2022-06-18 19:15:15 +0900

    groupmenuの中身をある程度実装

--- src/components/pages/GroupMenu.vue
+++ src/components/pages/GroupMenu.vue
@@ -1,5 +1,8 @@
 <template>
     <div>メニュー画面</div>
+    <div><router-link :to="{name:'SelectGroup'}">グループを選ぶ</router-link></div>
+    <div><router-link :to="{name:'JoinGroup'}">グループに入る</router-link></div>
+    <div><router-link :to="{name:'CreateGroup'}">グループを作る</router-link></div>
 </template>
 
 <script>
