89a5fd623636f87ef150c53ae315a191d7aebd1e
toshiki <yt.aeras412@outlook.jp>
2022-06-18 19:40:02 +0900

    グループ選択画面の作成

--- src/components/pages/SelectGroup.vue
+++ src/components/pages/SelectGroup.vue
@@ -1,7 +1,8 @@
 <template>
-    <div>グループを選択してください</div>
+    <div class="title">グループを選択してください</div>
     <div class="class-content" v-for="g in groups" v-bind:key="g.id">
-      <div class="class-name">{{g.name}}</div>
+      <!-- <div class="class-name"><router-link to="/groups/:groupId/members">{{g.name}}</router-link></div> -->
+      <router-link to="/groups/:groupId/members"><div class="class-name">{{g.name}}</div></router-link>
     </div>
 </template>
 
@@ -26,4 +27,16 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.title{
+ text-align: center;
+ font-size: 32px;
+ margin-top: 50px;
+}
+.class-name{
+  border: 1px solid black;
+  padding: 50px 50px;
+  margin: 30px 200px;
+  text-decoration: none;
+}
+
 </style>
