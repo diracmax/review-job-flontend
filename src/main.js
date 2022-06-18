5b042fc7227c160063bdb6776b88e4de939c23b9
diracmax <you@example.com>
2022-06-19 03:40:09 +0900

    デザインを少し追加

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router.js'
 
+import "bootstrap/dist/css/bootstrap.min.css"
+
 const application = createApp(App)
 application.use(router)
 application.mount('#app')
